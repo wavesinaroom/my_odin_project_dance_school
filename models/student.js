36ec4fb0ed6dcddf95fa388c8317a10ce641cfd0
wavesinaroom <jaureguij@javeriana.edu.co>
2023-11-28 12:34:36 -0500

    [Feature] Student model

--- models/student.js
+++ models/student.js
@@ -1,1 +1,13 @@
+const moongose = require("mongoose");
+const Schema = mongoose.Schema;
 
+const LessonSchema = new Schema({
+  first_name: {type: String, required: true, maxLength: 100},
+  family_name: {type: String, required: true, maxLength: 100},
+  username: {type: String, required: true, maxLength: 100},
+  password: {type: password, required: true, maxLength:100},
+  lessons: []
+
+});
+
+module.exports = mongoose.model("LessonSchema", LessonSchema);
