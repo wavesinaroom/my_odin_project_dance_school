98b51ef7f18d72a620dbf02d120513c41361372f
wavesinaroom <jaureguij@javeriana.edu.co>
2023-11-28 16:57:10 -0500

    [Task] Complete controller actions

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,30 +1,57 @@
 const Lesson = require("../models/lesson");
-const asynchandler = require("express-async-handler");
+const asyncHandler = require("express-async-Handler");
 
-exports.lesson_create_post = asynchandler(async(req,res,next)=>{
-
-});
-
-exports.lesson_delete_post = asynchandler(async(req,res,next)=>{
+exports.index = asyncHandler(async(req,res,next)=>{
 
 });
 
-exports.student_create_update = asynchandler(async(req,res,next)=>{
+exports.lesson_create_get = asyncHandler(async(req,res,next)=>{
+
+});
+exports.lesson_create_post = asyncHandler(async(req,res,next)=>{
 
 });
 
-exports.student_delete_update = asynchandler(async(req,res,next)=>{
+exports.lesson_delete_get = asyncHandler(async(req,res,next)=>{
 
 });
 
-exports.student_lesson_add_update = asynchandler(async(req,res,next)=>{
+exports.lesson_delete_post = asyncHandler(async(req,res,next)=>{
 
 });
 
-exports.student_lesson_remove_update = asynchandler(async(req,res,next)=>{
+exports.student_create_get = asyncHandler(async(req,res,next)=>{
 
 });
 
-exports.student_reset_password_update = asynchandler(async(req, res, nex)=>{
+exports.student_create_post = asyncHandler(async(req,res,next)=>{
+
+});
+
+exports.student_delete_get = asyncHandler(async(req,res,next)=>{
+
+});
+
+exports.student_lesson_add_get = asyncHandler(async(req,res,next)=>{
+
+});
+
+exports.student_lesson_add_post = asyncHandler(async(req,res,next)=>{
+
+});
+
+exports.student_lesson_remove_get = asyncHandler(async(req,res,next)=>{
+
+});
+
+exports.student_lesson_remove_post = asyncHandler(async(req,res,next)=>{
+
+});
+
+exports.student_reset_password_get = asyncHandler(async(req, res, nex)=>{
+
+});
+
+exports.student_reset_password_post = asyncHandler(async(req, res, nex)=>{
 
 });
