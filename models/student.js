ce30e6eefeeb692895a34e924f9885844f518685
wavesinaroom <jaureguij@javeriana.edu.co>
2023-11-28 15:48:23 -0500

    [Fix] Change schema name, add reference to lesson schema

--- models/student.js
+++ models/student.js
@@ -1,13 +1,12 @@
 const moongose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const LessonSchema = new Schema({
+const StudentSchema = new Schema({
   first_name: {type: String, required: true, maxLength: 100},
   family_name: {type: String, required: true, maxLength: 100},
   username: {type: String, required: true, maxLength: 100},
   password: {type: password, required: true, maxLength:100},
-  lessons: []
-
+  lessons: [{type: Schema.type.ObjectId, ref: "LessonSchema"}]
 });
 
-module.exports = mongoose.model("LessonSchema", LessonSchema);
+module.exports = mongoose.model("StudentSchema", StudentSchema);
