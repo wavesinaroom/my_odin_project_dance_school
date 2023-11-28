ba4b38aa7fbbaec7832893cc201866a447e9f657
wavesinaroom <jaureguij@javeriana.edu.co>
2023-11-28 17:24:46 -0500

    [Task] Send provisional messages to test routes

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -2,60 +2,60 @@ const Lesson = require("../models/lesson");
 const asyncHandler = require("express-async-Handler");
 
 exports.index = asyncHandler(async(req,res,next)=>{
-
+  res.send("NOT IMPLEMENTED: Admin main");
 });
 
 exports.lesson_create_get = asyncHandler(async(req,res,next)=>{
-
+  res.send("NOT IMPLEMENTED: Create lesson GET");
 });
 exports.lesson_create_post = asyncHandler(async(req,res,next)=>{
-
+  res.send("NOT IMPLEMENTED: Create lesson POST");
 });
 
 exports.lesson_delete_get = asyncHandler(async(req,res,next)=>{
-
+  res.send("NOT IMPLEMENTED: Create lesson GET");
 });
 
 exports.lesson_delete_post = asyncHandler(async(req,res,next)=>{
-
+  res.send("NOT IMPLEMENTED: Create lesson POST");
 });
 
 exports.student_create_get = asyncHandler(async(req,res,next)=>{
-
+  res.send("NOT IMPLEMENTED: Create lesson GET");
 });
 
 exports.student_create_post = asyncHandler(async(req,res,next)=>{
-
+  res.send("NOT IMPLEMENTED: Create lesson POST");
 });
 
 exports.student_delete_get = asyncHandler(async(req,res,next)=>{
-
+  res.send("NOT IMPLEMENTED: Create lesson GET");
 });
 
 exports.student_delete_post = asyncHandler(async(req,res,next)=>{
-
+  res.send("NOT IMPLEMENTED: Create lesson POST");
 });
 
 exports.student_lesson_add_get = asyncHandler(async(req,res,next)=>{
-
+  res.send("NOT IMPLEMENTED: Create lesson GET");
 });
 
 exports.student_lesson_add_post = asyncHandler(async(req,res,next)=>{
-
+  res.send("NOT IMPLEMENTED: Create lesson POST");
 });
 
 exports.student_lesson_remove_get = asyncHandler(async(req,res,next)=>{
-
+  res.send("NOT IMPLEMENTED: Create lesson GET");
 });
 
 exports.student_lesson_remove_post = asyncHandler(async(req,res,next)=>{
-
+  res.send("NOT IMPLEMENTED: Create lesson POST");
 });
 
 exports.student_reset_password_get = asyncHandler(async(req, res, nex)=>{
-
+  res.send("NOT IMPLEMENTED: Create lesson GET");
 });
 
 exports.student_reset_password_post = asyncHandler(async(req, res, nex)=>{
-
+  res.send("NOT IMPLEMENTED: Create lesson POST");
 });
