8cc12c863c3f8959e7321b1ecaab5192ba321cd6
wavesinaroom <jaureguij@javeriana.edu.co>
2023-11-28 16:32:48 -0500

    [Feature] Admin controller

---
+++ controllers/adminController.js
@@ -0,0 +1,30 @@
+const Lesson = require("../models/lesson");
+const asynchandler = require("express-async-handler");
+
+exports.lesson_create_post = asynchandler(async(req,res,next)=>{
+
+});
+
+exports.lesson_delete_post = asynchandler(async(req,res,next)=>{
+
+});
+
+exports.student_create_update = asynchandler(async(req,res,next)=>{
+
+});
+
+exports.student_delete_update = asynchandler(async(req,res,next)=>{
+
+});
+
+exports.student_lesson_add_update = asynchandler(async(req,res,next)=>{
+
+});
+
+exports.student_lesson_remove_update = asynchandler(async(req,res,next)=>{
+
+});
+
+exports.student_reset_password_update = asynchandler(async(req, res, nex)=>{
+
+});
