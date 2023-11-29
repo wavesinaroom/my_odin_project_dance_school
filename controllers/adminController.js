a8e153da8842d8df7147b939d034044e3badbfbd
wavesinaroom <jaureguij@javeriana.edu.co>
2023-11-29 11:39:02 -0500

    [Feature] Navigation menu

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -2,11 +2,11 @@ const Lesson = require("../models/lesson");
 const asyncHandler = require("express-async-handler");
 
 exports.index = asyncHandler(async(req,res,next)=>{
-  res.render("admin_create_lesson");
+  res.render("admin_main");
 });
 
 exports.lesson_create_get = asyncHandler(async(req,res,next)=>{
-  res.send("NOT IMPLEMENTED: Create lesson GET");
+  res.render("admin_create_lesson");
 });
 exports.lesson_create_post = asyncHandler(async(req,res,next)=>{
   res.send("NOT IMPLEMENTED: Create lesson POST");
@@ -20,19 +20,19 @@ exports.lesson_delete_post = asyncHandler(async(req,res,next)=>{
   res.send("NOT IMPLEMENTED: Delete lesson POST");
 });
 
-exports.student_create_get = asyncHandler(async(req,res,next)=>{
+exports.student_sign_up_get = asyncHandler(async(req,res,next)=>{
   res.send("NOT IMPLEMENTED: Create student GET");
 });
 
-exports.student_create_post = asyncHandler(async(req,res,next)=>{
+exports.student_sign_up_post = asyncHandler(async(req,res,next)=>{
   res.send("NOT IMPLEMENTED: Create student POST");
 });
 
-exports.student_delete_get = asyncHandler(async(req,res,next)=>{
+exports.student_remove_get = asyncHandler(async(req,res,next)=>{
   res.send("NOT IMPLEMENTED: Delete student GET");
 });
 
-exports.student_delete_post = asyncHandler(async(req,res,next)=>{
+exports.student_remove_post = asyncHandler(async(req,res,next)=>{
   res.send("NOT IMPLEMENTED: Delete student POST");
 });
 
@@ -44,11 +44,11 @@ exports.student_lesson_book_post = asyncHandler(async(req,res,next)=>{
   res.send("NOT IMPLEMENTED: Book student lesson POST");
 });
 
-exports.student_lesson_remove_get = asyncHandler(async(req,res,next)=>{
+exports.student_lesson_cancel_get = asyncHandler(async(req,res,next)=>{
   res.send("NOT IMPLEMENTED: Remove student lesson GET");
 });
 
-exports.student_lesson_remove_post = asyncHandler(async(req,res,next)=>{
+exports.student_lesson_cancel_post = asyncHandler(async(req,res,next)=>{
   res.send("NOT IMPLEMENTED: Remove student lesson POST");
 });
 
--- routes/admin.js
+++ routes/admin.js
@@ -11,17 +11,17 @@ router.post("/lesson/create", admin_controller.lesson_create_post);
 router.get("/lesson/delete", admin_controller.lesson_delete_get);
 router.post("/lesson/delete", admin_controller.lesson_delete_post);
 
-router.get("/student/create", admin_controller.student_create_get);
-router.post("/student/create", admin_controller.student_create_post);
+router.get("/student/signup", admin_controller.student_sign_up_get);
+router.post("/student/signup", admin_controller.student_sign_up_post);
 
-router.get("/student/delete", admin_controller.student_delete_get);
-router.post("/student/delete", admin_controller.student_delete_post);
+router.get("/student/remove", admin_controller.student_remove_get);
+router.post("/student/remove", admin_controller.student_remove_post);
 
 router.get("/student/book-lesson", admin_controller.student_lesson_book_get);
 router.post("/student/book-lesson", admin_controller.student_lesson_book_post);
 
-router.get("/student/delete-lesson", admin_controller.student_lesson_remove_get);
-router.post("/student/delete-lesson", admin_controller.student_lesson_remove_post);
+router.get("/student/cancel-lesson", admin_controller.student_lesson_cancel_get);
+router.post("/student/cancel-lesson", admin_controller.student_lesson_cancel_post);
 
 router.get("/student/password-reset", admin_controller.student_reset_password_get);
 router.post("/student/password-reset", admin_controller.student_reset_password_post);
