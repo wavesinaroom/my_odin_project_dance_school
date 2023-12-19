15d88fda123f7c2edca765cbeae42741eedfcb86
wavesinaroom <jaureguij@javeriana.edu.co>
2023-12-19 17:37:33 -0500

    [Feature] Student Log out

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.redirect("/login");
 });
+
+router.get('/logout', function(req,res,next){
+  req.logout(function(err){
+    if(err) {return next(err);}
+    res.redirect('/')
+  })
+})
 
 module.exports = router;
